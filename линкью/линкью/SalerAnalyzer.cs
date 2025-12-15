c717c63fb80a0801f0268305b7ddd57ffe50606c
blacktea123409874567 <150480352+blacktea123409874567@users.noreply.github.com>
2025-12-15 20:40:13 +0300

    Add files via upload

---
+++ линкью/линкью/SalerAnalyzer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace линкью
+{
+    public class SalerAnalyzer
+    {
+        private List<Sale> _sales;
+        public SalerAnalyzer(List<Sale> sales) 
+        {
+            _sales = sales ?? new List<Sale>();
+        }
+
+       /// <summary>
+       /// return total выручку
+       /// </summary>
+       /// <returns></returns>
+        public decimal GetToRevenue()
+        {
+            return _sales.Sum(x => x.Total);
+        }
+
+        public List<CategoryRevenue> GetToRevenueByCategory()
+        {
+            return _sales
+                .GroupBy(x => x.Category)
+                .Select(x => new CategoryRevenue
+                {
+                    Category = x.Key,
+                    Revenue = x.Sum(c =>c.Total)
+                })
+                .OrderByDescending(c => c.Revenue)
+                .ToList();
+        }
+
+        public List<CategoryAverage> GetToAverageByCategory()
+        {
+            return _sales
+                .GroupBy(x => x.Category)
+                .Select(x => new CategoryAverage
+                {
+                    Category = x.Key,
+                    AverageCheck = x.Average(c => c.Total)
+                })
+                .OrderByDescending(c => c.AverageCheck)
+                .ToList();
+        }
+        public List<CategoryCount> GetToCountByCategory()
+        {
+            return _sales
+                .GroupBy(x => x.Category)
+                .Select(x => new CategoryCount
+                {
+                    Category = x.Key,
+                    Count = x.Sum(c => c.Quantity)
+                })
+                .OrderByDescending(c => c.Count)
+                .ToList();
+        }
+
+
+
+        public List<Sale> Topthree()
+        {
+            return _sales.OrderByDescending(x => x.Total)
+                         .Take(3)
+                         .ToList<Sale>();
+        }
+    }
+}
